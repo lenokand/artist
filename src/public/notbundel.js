febd0b909061b052454e185f9c634a7f6618da67
lenokand <shtanko1991@gmail.com>
2022-02-28 22:05:15 +0300

    header

---
+++ src/public/notbundel.js
@@ -0,0 +1,19 @@
+var swiper_reviews = new Swiper(".swiper_main", {
+    // spaceBetween: 30,
+    // effect: "fade",
+    speed: 600,
+    // autoHeight: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: 'bullets',
+      clickable: 'true',
+    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //     // clickable: true,
+    // },
+    // autoplay: {
+    //     delay: 9000,
+    // },
+});
