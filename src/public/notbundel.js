177c4e6fe559dc76843bc72486ea9e9dfe907450
lenokand <shtanko1991@gmail.com>
2022-03-01 17:18:37 +0300

    main

--- src/public/notbundel.js
+++ src/public/notbundel.js
@@ -17,3 +17,22 @@ var swiper_reviews = new Swiper(".swiper_main", {
     //     delay: 9000,
     // },
 });
+
+var heart_left = document.getElementById('heart-left');
+var parallaxInstance = new Parallax(heart_left);
+var heart_right = document.getElementById('heart-right');
+var parallaxInstance = new Parallax(heart_right);
+
+
+const icons = document.querySelectorAll('.burger');
+const header = document.querySelector('header');
+const body = document.querySelector('body');
+icons.forEach (icon => {  
+  icon.addEventListener('click', (event) => {
+    icon.classList.toggle("open");
+    header.classList.toggle("open");
+    
+    body.classList.toggle("open");
+
+  });
+});
