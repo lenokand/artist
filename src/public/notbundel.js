ed1f36547472e38aac825ffdd2040c15d7a387d0
lenokand <shtanko1991@gmail.com>
2022-03-03 19:06:24 +0300

    footer

--- src/public/notbundel.js
+++ src/public/notbundel.js
@@ -138,3 +138,136 @@ show_btn.addEventListener('click', function(){
   show_btn.classList.add('hide')
 })
 
+
+
+
+// фиксированное меню
+
+  let menu = document.querySelector("menu")
+
+
+
+    window.addEventListener('scroll',function(){
+  
+      if( window.pageYOffset >= 200 && window.screen.width >= 1200){
+        
+        menu.classList.add('stiky')
+    
+      }
+      else{
+        menu.classList.remove('stiky')
+        
+      }
+    })
+    
+ 
+
+
+
+
+
+// $(function(){
+//  $(window).scroll(function(){
+//     var top = $(this).scrollTop();
+//     var elem = $('#top_nav');
+//     if (top+h_mrg < h_hght) {
+//      elem.css('top', (h_hght-top));
+//     } else {
+//      elem.css('top', h_mrg);
+//     }
+//   });
+// });
+
+
+// маска тел
+window.addEventListener("DOMContentLoaded", function () {
+  [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+    var keyCode;
+
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ __ __",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, ""),
+        new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+        });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+        function (a) {
+          return "\\d{1," + a.length + "}"
+        }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+      if (event.type == "blur" && this.value.length < 5) this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+
+  });
+
+});
+
+// запрет перемещения модального окна
+if(document.querySelector('[href="#modal-call"]')){
+  const filterGallary2 = Fancybox.bind('[href="#modal-call"]', {
+    // closeButton: "top",
+    dragToClose: false,
+    // Thumbs: false,
+    // Carousel: {
+    //     Dots: false,
+    // },
+  });
+  
+}
+if(document.querySelector('[href="#modal-call-second"]')){
+  const filterGallary2 = Fancybox.bind('[href="#modal-call-second"]', {
+    // closeButton: "top",
+    dragToClose: false,
+    // Thumbs: false,
+    // Carousel: {
+    //     Dots: false,
+    // },
+  });
+  
+}
+
+
+
+// кнопка наверх
+
+    //  Get the button
+    let mybutton = document.getElementById("btn-back-to-top");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+      scrollFunction();
+    };
+    
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        mybutton.style.display = "flex";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener("click", backToTop);
+    
+    function backToTop() {
+     window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    }
